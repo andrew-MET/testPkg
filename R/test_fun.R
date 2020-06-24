5f1f324988b2fd8f1106cee4e972b061593367eb
Andrew Singleton <andrewts@met.no>
2020-06-24 13:20:46 +0200

    add spaces

--- R/test_fun.R
+++ R/test_fun.R
@@ -12,16 +12,22 @@
 #' test_fun(seq(1, 4))
 #' test_fun("Yo!")
 test_fun <- function(x) {
+
   if (missing(x) || is.null(x) || is.na(x)) {
     stop("x must be something!")
   }
+
   if (is.list(x)) {
     stop("x can't be a list or data frame.")
   }
+
   if (is.numeric(x)) {
     x <- formatC(x, width = 5, flag = "0")
   }
+
   x <- paste(x, collapse = ", ")
   message("You entered '", x, "'.")
+
   x
+
 }
